fc5d6087f61b23d2a08b63399e889625ec50c764
pk <pk_server@pkserver@quickcom.com>
2025-03-24 17:16:50 +0000

    user ,file analyzer

--- rootEsc/modules/analyzers/file.py
+++ rootEsc/modules/analyzers/file.py
@@ -1,12 +1,14 @@
+from subprocess import run
+
 class FileAnalysis():
-    def __init__(self):
-        pass
-    
+
     def get_suid_bins(self):
-        pass
+        suid_bins_output=run("find / -type f -perm -04000 2>/dev/null",capture_output=True,shell=True)
+        return suid_bins_output
     
     def get_sgid_bins(self):
-        pass
+        sgid_bins_output=run("find / -type f -perm -02000 2>/dev/null",capture_output=True,shell=True)
+        return sgid_bins_output
     
     def get_writable_permission_dirs_in_env_path(self):
         pass
--- rootEsc/modules/analyzers/user.py
+++ rootEsc/modules/analyzers/user.py
@@ -1,16 +1,20 @@
+from subprocess import run
+
 class UserAnalysis():
     def __init__(self):
         pass
     
     def get_all_users(self):
-        pass
+        all_users_output=run("cat /etc/passwd ",capture_output=True,shell=True)
+        return all_users_output
     
-    def get_user_id(self):
-        pass
+    def get_current_user_id(self):
+        user_id_output=run("id",capture_output=True,shell=True)
+        return user_id_output
     
     def get_sudo_privileges(self):
-        pass
+        sudo_privilege_output=run("sudo -nl ",capture_output=True,shell=True)
+        return sudo_privilege_output
     
-    def get_user_owned_files(self):
+    def get_user_permited_files(self):
         pass
-    
