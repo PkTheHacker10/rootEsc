6313d7c801d244a5883fd4ad0c643e4b9a65fac1
pk <pk_server@pkserver@quickcom.com>
2025-03-24 15:39:31 +0000

    system ,file,user analyzers funtions template done

---
+++ rootEsc/modules/analyzers/user.py
@@ -0,0 +1,16 @@
+class UserAnalysis():
+    def __init__(self):
+        pass
+    
+    def get_all_users(self):
+        pass
+    
+    def get_user_id(self):
+        pass
+    
+    def get_sudo_privileges(self):
+        pass
+    
+    def get_user_owned_files(self):
+        pass
+    
